dbe8edf33cba1398c8e061a978c9f39c87a7fd21
Jared Lalmansingh <jared.lalmansingh@wustl.edu>
2021-03-26 12:15:10 -0500

    Added comments that better explain certain parts of the code.

--- camparitraj/tests/test_ctutils.py
+++ camparitraj/tests/test_ctutils.py
@@ -30,14 +30,22 @@ def test_mkl_set_num_threads():
             other = filepath[:]
 
     if mkl_path is not None:
+        # Traditional UNIX-like systems will have shared objects available.
         if 'bsd' in sys.platform or 'lin' in sys.platform:
             mkl_rt = ctypes.CDLL('libmkl_rt.so')
+        
+        # Darwin / Apple uses `*.dylib` by default for included Intel compiler libraries.
+        # Traditional UNIX-like shared objects can be created (`*.so`), but are more
+        # represented in third-party libraries. This is a more dynamic way of finding
+        # the MKL library and using it on a Mac that has an Intel compiler installed.
         elif sys.platform == 'darwin':
             mkl_rt = ctypes.CDLL(mkl_path)
         ctutils.mkl_set_num_threads(num_threads)
         num_threads_set = mkl_rt.mkl_get_max_threads
         assert num_threads_set == num_threads
 
+    # Most BLAS implementations use OpenBLAS, this section will likely be used the most
+    # in production.
     elif openblas_path is not None:
         openblas_lib = ctypes.cdll.LoadLibrary(openblas_path)
         openblas_lib.openblas_set_num_threads(num_threads)
@@ -54,7 +62,7 @@ def test_validate_keyword_option():
         ctutils.validate_keyword_option(mode, allowed_modes, 'mode')
         ctutils.validate_keyword_option(mode, allowed_modes, 'mode', 'Unsupported mode.')
 
-    # Attempt a test which will fail.
+    # Attempt a test which will fail - i.e. mismatched keyword.
     keyword_not_found = False
     try:
         ctutils.validate_keyword_option('invalid_mode', allowed_modes, 'mode')
@@ -62,10 +70,19 @@ def test_validate_keyword_option():
         keyword_not_found = True
     assert keyword_not_found == True
 
-    # Attempt a test which will fail.
+    # Attempt a test which will fail, but including a custom string error message.
     keyword_not_found = False
     try:
         ctutils.validate_keyword_option('invalid_mode', allowed_modes, 'mode', 'Unsupported mode.')
     except CTException as e:
+        keyword_not_found = True
+    assert keyword_not_found == True
+
+
+    # Attempt a test which will fail, including a non-string error message.
+    keyword_not_found = False
+    try:
+        ctutils.validate_keyword_option('invalid_mode', allowed_modes, 'mode', 123)
+    except RuntimeError as e:
         keyword_not_found = True
     assert keyword_not_found == True
