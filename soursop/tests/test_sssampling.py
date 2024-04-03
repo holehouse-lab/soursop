1e964b604e5d5e4cde9759bfd86f33d0318eb31e
Jeff Lotthammer <jeffrey.lotthammer@gmail.com>
2024-04-02 19:26:19 -0500

    add 2d sssampling hellinger regression test

--- soursop/tests/test_sssampling.py
+++ soursop/tests/test_sssampling.py
@@ -9,6 +9,7 @@ from soursop.sstools import find_trajectory_files
 import soursop
 import os
 
+
 def test_hellingers_distance():
     p = np.array([1,0])
     q = np.array([0,1])
@@ -39,14 +40,14 @@ def test_sampling_quality():
                     top_file=wt_top_paths,
                     ref_top=ev_top_paths,
                     method="1D angle distributions")
-
+    
     hellingers = np.array([[[0.24167835, 0.19611402],
                             [0.24688875, 0.24731097],
                             [0.24903448, 0.20723213]],
                             [[0.13862987, 0.1848602 ],
                              [0.159583  , 0.18751601],
                              [0.1429838 , 0.13090839]]])
-
+    
     assert np.all(alanine_qual.psi_angles.shape == (3, 2, 500))
     assert np.allclose(alanine_qual.psi_angles[0][0][0], -39.17526)
     assert np.allclose(alanine_qual.phi_angles[0][0][0], -144.49619)
@@ -78,3 +79,24 @@ def test_sampling_quality():
                         alanine_qual.compute_pdf(alanine_qual.ref_psi_angles[0], 
                         bins=bins)
                     )
+
+
+
+def test_hellingers_2d():
+    data_dir = soursop.get_data('test_data')
+    wt_data = os.path.join(data_dir,"sampling_quality/WT")
+    ev_data = os.path.join(data_dir,"sampling_quality/EV")
+    wt_traj_paths, wt_top_paths  = find_trajectory_files(wt_data,3)
+    ev_traj_paths, ev_top_paths = find_trajectory_files(ev_data,3)
+    
+    hellingers_2d = np.array([[0.44302328, 0.42337237],
+                            [0.46023975, 0.46853325],
+                            [0.42439771, 0.42886934]])
+    
+    alanine_qual = SamplingQuality(wt_traj_paths,
+                    ev_traj_paths, 
+                    top_file=wt_top_paths,
+                    ref_top=ev_top_paths,
+                    method="2D angle distributions")
+    
+    assert np.allclose(alanine_qual.compute_dihedral_hellingers(), hellingers_2d)
